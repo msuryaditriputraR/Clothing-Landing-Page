8ba1cfa285df6a99056ad89ba6ed5ff886ee8991
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-10-07 18:43:06 +0800

    change background color  header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,13 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+    const header = document.getElementById("header");
 
+    this.scrollY >= 50
+        ? header.classList.add("scroll-header")
+        : header.classList.remove("scroll-header");
+};
+
+window.addEventListener("scroll", scrollHeader);
 /*=============== SWIPER PRODUCTS ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
